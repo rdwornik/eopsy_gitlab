2ed261f853ce26cb89e56f73fccabba4596b9c30
robertdwornik <robert.dwornik@outlook.com>
2019-05-29 18:08:50 +0200

    done

--- task6/copy.c
+++ task6/copy.c
@@ -28,6 +28,7 @@ int main(int argc, char** argv)
     int fd;
     int fo;
 
+    //zczytujemy flagi za pomocą funkcji getopt
     while ((opt = getopt(argc,argv,":m:h")) != -1)
     {
         switch (opt)
@@ -36,25 +37,27 @@ int main(int argc, char** argv)
             help();
             exit(EXIT_SUCCESS);
         case 'm':
-            flag = 1;
+            flag = 1; //ustawiamy flage na jeden jeśli ta opcja została wybrana
             index = strlen(optarg) + 1; //index of the second argument
-            arg1 = optarg;
-            arg2 = optarg + index;
+            arg1 = optarg;          //optarg zwraca tablice charów
+            arg2 = optarg + index; //przesuwamy się o długość pierwszego stringa by zczytać drugi argument
             
+            //sprawdzamy czy użytkownik podał drugi argument
             if(strncmp(arg2,"CLUTTER_IM_MODULE=xim",strlen(arg2)) != 0)
             {
-                printf("i am following -m: first arg %s secodn %s \n", arg1,arg2);
-
-                if((fd = open(arg1, O_RDONLY)) == -1){
+            //otwieram plik na inputcie flaga read only więcej uprawnień nam nie potrzeba    
+                if((fd = open(arg1, O_RDONLY)) == -1)
+                {
                     perror("no such a input file\n");
                     exit(EXIT_FAILURE);
                 }
-
-                if((fo = open(arg2, O_WRONLY | O_TRUNC)) == -1){
+            //otwieram plik do którego będziemy tylko nadpisywać, flaga O_TRUNC czyści plik z zawartości aby nie doszło do dopisania
+                if((fo = open(arg2, O_WRONLY | O_TRUNC)) == -1)
+                {
                     perror("no such a output file\n");
                     exit(EXIT_FAILURE);
                 }
-            
+            //wywołanie funkcij    
                 using_read_write(fd,fo);
             }
             else
@@ -74,18 +77,20 @@ int main(int argc, char** argv)
     //no option choses
     if(flag != 1)
     {
-       arg1 = argv[optind];
-       optind++;
-       arg2 = argv[optind];
-       printf("arg1: %s arg2: %s \n", arg1,arg2);
-      
-        if((fd = open(arg1,O_RDWR)) == -1)
+        //w optind znajduje się index kolejnych argumentów
+        arg1 = argv[optind];  
+        optind++;
+        arg2 = argv[optind];
+
+        //znowu otwieramy plik tylko zczytywanie będzie nam potrzebne      
+        if((fd = open(arg1,O_RDONLY)) == -1)
         {
             perror("no such a input file\n");
             exit(EXIT_FAILURE);
         }
-
-        if((fo= open(arg2,O_RDWR)) == -1)
+        //otwieramy plik tym razem uprawnienia do zczytywania i nadpisywania będą nam potrzbne ponieważ używają mmap
+        //inaczej dostaniemy komunikat o braku uprawnień do odczytu pliku.
+        if((fo = open(arg2,O_RDWR| O_TRUNC)) == -1)
         {
             perror("no such a input file\n");
             exit(EXIT_FAILURE);
@@ -96,35 +101,50 @@ int main(int argc, char** argv)
 
 void using_mmap(int fd_from, int fd_to)
 {
+    //stuct stat jest to zmienna systemowa w której przechowujemy statystki pliku z funkcji fstat
     struct stat sb;
     char *input;
     char *out;
 
+    //funkcja stat zbiera statyski takie jak wielkość czy też poziom uprawnień nam potrzeba tylko wielkość pliku
     if(fstat(fd_from,&sb) == -1)
     {
         perror("can't get file size \n");
         exit(EXIT_FAILURE);
     }
 
-    if((input = mmap(NULL,sb.st_size,PROT_READ | PROT_WRITE, MAP_SHARED, fd_from,0)) == MAP_FAILED)
+    //mmap zwraca nam wskaźnik do pierwszego indexu bloku pamięci w którym znajduje się nasz input plik
+    //nadajemy mu uprawnienia do sczytywania ponieważ więcej nam nie będzie potrzebne a mapowanie jest prywatne ponieważ nie ma potrzeby
+    //dzielenie się z innymi procesami tym fragmentem pamięci
+    //sb.st_size przechowuje wielkość pliku
+    if((input = mmap(NULL,sb.st_size,PROT_READ, MAP_PRIVATE, fd_from,0)) == MAP_FAILED)
     {
         perror("Mapping input file failed \n");
         exit(EXIT_FAILURE);
     }
 
+    //jako że nasz wyczyszczony output plik ma wielkość zero nie możemy przypisać bloku pamięci o większej wielkość do bloku o wielkości większej
+    //dlatego rozszerzamy wielkość output pliku do wielkości naszego input pliku
     ftruncate(fd_to,sb.st_size);
 
+    //tak jak poprzednio mmap zwracam nam wskaźnik do miejsce w pamięci w którym znajduje się nasz output plik 
+    //tym razem nadajemy inne uprawnienia jako że będzięmy zapisywać kopie input do jakiegoś miejsca w pamięci 
+    //musi on mieć uprawnienia do zapisu, jednocześnie musi mieć ustawiony flage MAP_SHARED aby pamięć mogła być jawnie przypisana 
+    //inaczej dostaniemy hash 
     if((out = mmap(NULL,sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_to,0)) == MAP_FAILED)
     {
         perror("Mapping output file failed \n");
         exit(EXIT_FAILURE);
     }
 
+    //kopiujemy miejsce w pamięci w którym znajduje się nasz input plik do miejsca w pamięci w którym znajduje się nasz output plik
     memcpy(out,input,sb.st_size);
 
+    //munmap usuwanie mapowanie dla określonego przedziału jest to dobra praktyka gdy zakończymy mapowanie
     munmap(input,sb.st_size);
     munmap(out,sb.st_size);
 
+    //zamykam bezpiecznie pliki aby użytkownik mógł mieć donich dostęp po zakończeniu działania programu
     if((close(fd_from)) == -1 || close(fd_to) == -1)
     {
         perror("error on closing occured\n");
@@ -135,25 +155,29 @@ void using_mmap(int fd_from, int fd_to)
 void using_read_write(int fd_from, int fd_to)
 {
     
-    char buffer[MAX_BUFFER_SIZE];
+    char buffer[MAX_BUFFER_SIZE]; //buffor przechowuje chary z czytanego pliku
     ssize_t size;
 
+    //czytamy z pliku chary i wpisujemy do buffora
+    //funkcja read zwraca wielkość pliku
     if((size = read(fd_from,&buffer,sizeof(buffer))) == -1)
     {
         perror("error occured durig reading buffer\n");
         exit(EXIT_FAILURE);
     }
-
+    //sprawdzamy czy bufor nie został przepełniony
     if(!(size < sizeof(buffer)))
         printf("size of file is to big \n");
     else
     {
+        //zapisujemy chary z buffora do pliku
         if(write(fd_to,&buffer,size) == -1)
         {
             perror("error occured durig writing to file from buffer\n");
             exit(EXIT_FAILURE);
         }
-        
+
+        //zamykam bezpiecznie pliki aby użytkownik mógł mieć donich dostęp po zakończeniu działania programu
         if((close(fd_from)) == -1 || close(fd_to) == -1)
         {
             perror("error on closing occured\n");
@@ -164,6 +188,7 @@ void using_read_write(int fd_from, int fd_to)
 
 void help()
 {
+    //to samo co dla funkcji using_mmap
     int fd;
     struct stat sb;
     char *input;
@@ -185,6 +210,14 @@ void help()
         perror("Mapping input file failed \n");
         exit(EXIT_FAILURE);
     }
+
+    //wykorzystujemy fakt że map zwracam na wskaźnik do początku miejsca w pamięci w którym znajdują się nasze zasoby 
+    for(int i = 0; i < sb.st_size; i++)
+    {
+        printf("%c", input[i]);
+    }
+
+    printf("\n");
     
     if((close(fd)) == -1)
     {
