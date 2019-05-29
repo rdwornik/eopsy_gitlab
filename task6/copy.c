b1b6cdfd7efe7c8c618b3526ee9a1902510eb579
robertdwornik <robert.dwornik@outlook.com>
2019-05-29 09:30:10 +0200

    done

--- task6/copy.c
+++ task6/copy.c
@@ -12,12 +12,11 @@
 
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
+#define MAX_BUFFER_SIZE 100
 
 void help();
 void using_mmap(int fd_from, int fd_to);
 void using_read_write(int fd_from, int fd_to);
-
-
 
 int main(int argc, char** argv){
     int opt;
@@ -49,7 +48,7 @@ int main(int argc, char** argv){
                     exit(EXIT_FAILURE);
                 }
 
-                if((fo = open(arg2, O_WRONLY)) == -1){
+                if((fo = open(arg2, O_WRONLY | O_TRUNC)) == -1){
                     perror("no such a output file\n");
                     exit(EXIT_FAILURE);
                 }
@@ -90,31 +89,29 @@ int main(int argc, char** argv){
     }  
 }
 
-void help(){
-    
-}
-
 void using_mmap(int fd_from, int fd_to){
     struct stat sb;
+    char *input;
+    char *out;
+
     if(fstat(fd_from,&sb) == -1){
         perror("can't get file size \n");
         exit(EXIT_FAILURE);
     }
 
+  if((input = mmap(NULL,sb.st_size,PROT_READ, MAP_PRIVATE, fd_from,0)) == MAP_FAILED){
+      perror("Mapping input file failed \n");
+      exit(EXIT_FAILURE);
+  }
+  if((out = mmap(NULL,sb.st_size,PROT_READ | PROT_WRITE, MAP_SHARED, fd_to,0)) == MAP_FAILED){
+      perror("Mapping output file failed \n");
+      exit(EXIT_FAILURE);
+  }
+  
+  memcpy(out,input,sb.st_size);
 
-
-    printf("file size is %ld\n", sb.st_size);
-
-  char *input = mmap(NULL,sb.st_size,PROT_READ | PROT_WRITE, MAP_PRIVATE, fd_from,0);
- 
-
- 
-  char *out = mmap(NULL,sb.st_size,PROT_READ | PROT_WRITE, MAP_SHARED, fd_to,0);
- 
-  for(int i = 0; i < sb.st_size; i++)
-  printf("%c",input[i]);
-//   printf("\n");
-   memcpy(out,input,sb.st_size);
+  munmap(input,sb.st_size);
+  munmap(out,sb.st_size);
 
    if((close(fd_from)) == -1 || close(fd_to) == -1){
             perror("error on closing occured\n");
@@ -122,13 +119,11 @@ void using_mmap(int fd_from, int fd_to){
         }
 }
 
-
-
-
 void using_read_write(int fd_from, int fd_to){
     
-    char buffer[100];
+    char buffer[MAX_BUFFER_SIZE];
     ssize_t size;
+
     if((size = read(fd_from,&buffer,sizeof(buffer))) == -1){
         perror("error occured durig reading buffer\n");
         exit(EXIT_FAILURE);
@@ -147,5 +142,15 @@ void using_read_write(int fd_from, int fd_to){
             exit(EXIT_FAILURE);
         }
     }
+}
+
+void help(){
+    int fd;
+     if((fd = open("help.txt", O_RDONLY)) == -1){
+                    perror("no such a input file\n");
+                    exit(EXIT_FAILURE);
+                }
+        
+
 }
 
